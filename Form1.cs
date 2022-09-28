4210ae3f759ed0d9b6bff8d3384728e42ea3e803
VaccaroniAlberto <alberto.vaccaroni.studenti@isii.it>
2022-09-28 08:39:10 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_git_2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("perchè");
+        }
+    }
+}
